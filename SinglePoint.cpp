879ad69faa950c76a078d9ee808c69c3cbfec724
Marian Heil <48517413+marianheil@users.noreply.github.com>
2019-02-15 17:05:44 +0000

    More cuts & cleanup

--- SinglePoint.cpp
+++ SinglePoint.cpp
@@ -50,9 +50,25 @@ namespace {
     return 1./2.*log(1.*(p[3]+p[2])/(p[3]-p[2]));
   }
 
-  double get_E(double const p0, double const p1, double const p2, double const m = 0.){
-    return sqrt(+p0*p0+p1*p1+p2*p2+m*m);
+  double phi(vec4 const & p) {
+    double phi = atan2(p[1],p[2]);
+    if (phi < 0.0) phi += 2.*M_PI;
+    if (phi >= 2.*M_PI) phi -= 2.*M_PI;
+    return phi;
   }
+
+  double dR(vec4 const & a, vec4 const & b){
+    return std::abs(std::abs(rap(a)-rap(b))-std::abs(phi(a)-phi(b)));
+  }
+
+  double get_E2(double const p0, double const p1, double const p2, double const m2 = 0.){
+    return sqrt(+p0*p0+p1*p1+p2*p2+m2);
+  }
+
+  double get_E(double const p0, double const p1, double const p2, double const m = 0.){
+    return get_E(p0,p1,p2,m*m);
+  }
+
 
   std::vector<vec4> possible_mom(
       double const mass,double const maxp,double const minpt){
@@ -72,13 +88,13 @@ namespace {
   }
 
   mom_map possible_mom_map(
-      double const mass,double const maxp,double const minpt){
+      double const mass2,double const maxp,double const minpt){
     mom_map results;
     for(double i0=-maxp; i0<maxp;++i0){
       for(double i1=-maxp; i1<maxp;++i1){
         if(i0*i0+i1*i1>minpt*minpt){
           for(double i2=-maxp; i2<maxp;++i2){
-            const double E(get_E(i0, i1, i2, mass));
+            const double E(get_E2(i0, i1, i2, mass2));
             if(is_int(E))
               results[i0][i1].emplace_back(std::array<double,2>{i2,E});
           }
@@ -88,22 +104,26 @@ namespace {
     return std::move(results);
   }
 
-  std::array<vec4, 2> construct_incoming(std::vector<vec4> const & outgoing){
+  std::array<vec4, 2> construct_incoming(std::vector<vec4>::const_iterator const begin, std::vector<vec4>::const_iterator const end){
     double xa = 0.;
     double xb = 0.;
-    for(auto const & out: outgoing){
-      xa += out[3] - out[2];
-      xb += out[3] + out[2];
+    for(auto out = begin; out<end; ++out){
+      xa += (*out)[3] - (*out)[2];
+      xb += (*out)[3] + (*out)[2];
     }
     return {{{{0,0,-xa/2.,xa/2.}}, {{0,0,xb/2.,xb/2.}}}};
+  }
+
+  std::array<vec4, 2> construct_incoming(std::vector<vec4> const & outgoing){
+    return construct_incoming(outgoing.begin(), outgoing.end());
   }
 
   template<class T>
   void print_array(T const & a){
     std::cout << "{";
-    for(const auto p: a)
-      std::cout << p << ", ";
-    std::cout << "}";
+    for(auto p=a.begin(); p<a.end()-1; ++p)
+      std::cout << *p << ", ";
+    std::cout << *(a.end()-1) << "}";
   }
 
   std::vector<t_result> find_possible(double const maxp, bool const check_in){
@@ -172,14 +192,22 @@ namespace {
     return ret;
   }
 
-  std::vector<t_result> find_possible_map(double const maxp, bool const check_in){
-    const auto p_parton{possible_mom_map(0,maxp,30)};
-    const auto p_higgs{possible_mom_map(126,maxp,0)};
+  size_t find_possible_map(double const maxp, bool const check_in){
+    constexpr double max_rap = 5.; // maximal rapidity of jets
+    constexpr double min_Rjj = 0.4; // minimal R distance between 2 jets
+    constexpr double min_ptj = 30.; // minimal jet p_t
+    constexpr double max_Ecm = 10000.; // maximal center of mass Energy
+    constexpr double higgs_m2 = 88.*88.*2.; // mass square of the Higgs boson
+
+    const auto p_parton{possible_mom_map(0,maxp,min_ptj)};
+    const auto p_higgs{possible_mom_map(higgs_m2,maxp,0)};
+
     std::cout << p_parton.size() << " " << p_higgs.size() << "\nreal: "
       << mom_map_size(p_parton) << " " << mom_map_size(p_higgs) << std::endl;
-    std::vector<t_result> results;
+    size_t results=0;
     for(const auto & ax: p_higgs)
       for(const auto & bx: p_parton){
+        // enforce different px for higgs and the first jet (make it look more random)
         if(bx.first == ax.first) continue;
         for(const auto & cx: p_parton){
           // TODO make this calls nicer
@@ -195,31 +223,35 @@ namespace {
                 if(dy == dx->second.end())
                   continue;
 
+                t_result particles;
+                particles.resize(6);
                 for(const auto & azE: ay.second){
+                  particles[0]=vec4{ax.first,  ay.first,  azE[0], azE[1]};
+                  if(rap(particles[0]) > max_rap) continue;
                   for(const auto & bzE: by.second){
+                    particles[1]=vec4{bx.first,  by.first,  bzE[0], bzE[1]};
+                    if(rap(particles[1]) > max_rap) continue;
                     for(const auto & czE: cy.second){
+                      particles[2]=vec4{cx.first,  cy.first,  czE[0], czE[1]};
+                      if( dR( particles[2], particles[1] ) < min_Rjj) continue;
+                      if(rap(particles[2]) > max_rap) continue;
                       for(const auto & dzE: dy->second){
-                        t_result particles;
-                        particles.emplace_back(vec4{ax.first,  ay.first,  azE[0], azE[1]});
-                        if(rap(particles.back()) > 5.) continue;
-                        particles.emplace_back(vec4{bx.first,  by.first,  bzE[0], bzE[1]});
-                        if(rap(particles.back()) > 5.) continue;
-                        particles.emplace_back(vec4{cx.first,  cy.first,  czE[0], czE[1]});
-                        if(rap(particles.back()) > 5.) continue;
-                        particles.emplace_back(vec4{dx->first, dy->first, dzE[0], dzE[1]});
-                        if(rap(particles.back()) > 5.) continue;
-                        auto const incoming(construct_incoming(particles));
+                        particles[3]=vec4{dx->first, dy->first, dzE[0], dzE[1]};
+                        if(rap(particles[3]) > max_rap) continue;
+                        if(( dR(particles[3], particles[2]) < min_Rjj) || ( dR(particles[3], particles[1]) < min_Rjj)) continue;
+                        auto const incoming(construct_incoming(particles.begin(), particles.end()-2));
                         if(!check_in ||
                             ( is_int(incoming)
-                              && (2*(incoming[0][3]*incoming[0][3]+incoming[1][3]*incoming[1][3])<10000*10000) )
+                              && (2*(incoming[0][3]*incoming[0][3]+incoming[1][3]*incoming[1][3])<max_Ecm*max_Ecm) )
                         ){
-                          particles.insert( particles.end(), incoming.begin(), incoming.end() );
+                          particles[particles.size()-2]=std::move(incoming[0]);
+                          particles[particles.size()-1]=std::move(incoming[1]);
                           std::cout << "POSSIBLE FOUND" << std::endl;
                           for(auto const & p: particles){
                             print_array(p);
-                            std::cout << std::endl;
+                            std::cout << " y=" << rap(p) << " phi=" << phi(p) << std::endl;
                           }
-                          results.emplace_back(std::move(particles));
+                          ++results;
                         }
                       }
                     }
@@ -228,21 +260,13 @@ namespace {
               }
         }
       }
-    return std::move(results);
+    return results;
   }
 }
 
-/**
- * optimisation:
- * 1. for "d" use (unordered) map( x->map( y->vector(array(z,E)) ) )
- * 2. if(d.find(a[0]+b[0]+c[0]) != d.end()) ...
- * 3. if(d.find(a[1]+b[1]+c[1]) != d.end()) ...
- *
- */
-
 int main(){
-  std::vector<t_result> results{find_possible_map(100, true)};
-  std::cout << "Map Found " << results.size() << " possible momenta" << std::endl;
+  auto results{find_possible_map(50, true)};
+  std::cout << "Map Found " << results << " possible momenta" << std::endl;
   // std::vector<t_result> results_normal{find_possible(200, false)};
   // std::cout << "Found " << results_normal.size() << " possible momenta" << std::endl;
 }
